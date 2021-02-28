d822357c486fa2cbd476370d766760892d4000cc
konnorov <konnorov1@gmail.com>
2021-02-28 13:55:25 +0300

    Updates

--- static/js/4.38ec598e.chunk.js
+++ static/js/4.38ec598e.chunk.js
@@ -1,2 +1,1 @@
 (this.webpackJsonprequests=this.webpackJsonprequests||[]).push([[4],{172:function(s,t,e){"use strict";e.r(t);var i=e(170),u=e(171);i.init(),i.add(u),t.default=i}}]);
-//# sourceMappingURL=4.38ec598e.chunk.js.map
